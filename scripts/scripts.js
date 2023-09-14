086ff7e25e4f8404e7ca6006ede2ad58bd388618
Valentina Valverde <vvgreenville@gmail.com>
2023-09-14 14:50:57 -0400

    Created a drop-down menu in class.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,48 +1,48 @@
 'use strict';
 
-
-//longhand: API Practice
-document.addEventListener('DOMContentLoaded', function(){
-    console.log("DOM Content Loaded");
-
-    const chuckQuote = document.getElementById('chuckQuote');
-
-    const fetchQuote = fetch('https://api.chucknorris.io/jokes/random?category=dev');
-    const quoteResponse = fetchQuote.then(function(response){
-        console.log("RESPONSE:", response);
-        return response.json();
-    });
-    const jsonResponse = quoteResponse.then(function(data){
-        console.log("DATA:", data);
-        showQuote(data.value, chuckQuote);
-        return data;
-    });
-    console.log("JSON:", jsonResponse);
-});
-
-function showQuote(quote, element){
-    console.log("QUOTE:", quote);
-    element.innerText = quote;
-};
-
+const categoryListForm = document.getElementById('categoryListForm');
 
 //shothand API: chaining .then!
 document.addEventListener('DOMContentLoaded', function () {
     console.log("DOM Content Loaded");
 
     const chuckQuote = document.getElementById('chuckQuote');
+    const apiUrl = 'https://api.chucknorris.io/jokes/random';
+    get(apiUrl).then(function(response){
+        console.log("RESPONSE:", response);
+        showQuote(response.value, chuckQuote);
+    })
 
-    fetch('https://api.chucknorris.io/jokes/random')
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            showQuote(data.value, chuckQuote);
-            return data;
-        });
+    const categoriesUrl = 'https://api.chucknorris.io/jokes/categories';
+    get(categoriesUrl).then(function(response){
+        console.log("CATEGORIES:", response);
+        generateCategoryList(response);
+    })
 });
 
 function showQuote(quote, element) {
     console.log("QUOTE:", quote);
     element.innerText = quote;
 };
+
+
+
+function generateCategoryList(categoryArray){
+    console.log(categoryArray);
+    const selectElement = document.createElement('select');
+    categoryArray.map(function (category){
+        //create option element
+        const option = document.createElement('option');
+        //define option atributes
+        option.value - category;
+        option.text = category;
+        //append option to <select>
+        selectElement.appendChild(option);
+    });
+    //append the <select> to <form>
+    categoryListForm.append(selectElement);
+};
+
+categoryListForm.addEventListener('submit', function(){
+    
+})
